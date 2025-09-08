7f18c80aff69d845998b3e4ceff8ebf7343d0554
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-09-08 12:59:18 +0300

    add les2

---
+++ les2/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+{
+const productName = "LG 2589 36''";
+const count = 12;
+const category = 'Телевизор'; 
+const cost = 1359.21;
+
+console.log(`Товар ${productName} на сумму ${count*cost} руб`);
+}
+
+{
+const productName = "Минская марка 25%";
+const count = 46;
+const category = 'Сметана'; 
+const cost = 3.09;
+
+console.log(`Товар ${productName} на сумму ${count*cost} руб`);
+}
