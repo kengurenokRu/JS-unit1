cd49f60a2859888d176e0f009047be91df2659bf
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-09-08 12:31:10 +0300

    initial

---
+++ les1-2/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const productName = "LG 2589 36''";
+const count = 12;
+const category = 'Телевизор'; 
+const cost = 1359.21;
+
+console.log(`Товар ${productName} на сумму ${count*cost} руб`);
