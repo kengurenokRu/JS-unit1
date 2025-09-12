821b22db61711c1126154811cf42dbd0cc8f1061
kengrenokRu <nastassiadunaeva@gmail.com>
2025-09-12 12:59:51 +0300

    add les4-3

--- les4/tasks.js
+++ les4/tasks.js
@@ -1,14 +1,12 @@
 const rain = Math.round(Math.random());
 
-if (rain) {
-  console.log('Пошёл дождь. Возьмите зонт!');
-}
-else {
-  console.log('Дождя нет!');
-}
+if (rain) { console.log('Пошёл дождь. Возьмите зонт!'); }
+else { console.log('Дождя нет!'); }
 
-
-
-
+const ex1 = Number(prompt('Введите кол-во баллов по математике:'));
+const ex2 = Number(prompt('Введите кол-во баллов по русскому языку:'));
+const ex3 = Number(prompt('Введите кол-во баллов по информатике:'));
+if (ex1 + ex2 + ex3 >= 265) { console.log('Поздравляю, вы поступили на бюджет!'); }
+else { console.log('Сожалеем, вы не поступили на бюджет!'); }
 
 
