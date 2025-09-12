690bbe033cc223642fa4c27d8cb7633391784569
kengrenokRu <nastassiadunaeva@gmail.com>
2025-09-12 14:05:56 +0300

    add task02

---
+++ les5/task02.js
@@ -0,0 +1,10 @@
+const myStr = prompt('Введите вашу строку:');
+
+const result = (str) => {
+  let strTemp = str;
+  strTemp = strTemp.toLowerCase();
+  strTemp = strTemp.charAt(0).toUpperCase() + strTemp.slice(1);
+  return strTemp
+};
+
+console.log(result(myStr));
