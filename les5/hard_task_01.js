dba58fecd6fc0c778708fc791435ec2c353689d5
kengrenokRu <nastassiadunaeva@gmail.com>
2025-09-12 14:31:08 +0300

    add hard_task_01

---
+++ les5/hard_task_01.js
@@ -0,0 +1,14 @@
+const number1 = Number(prompt('Введите первое число:'));
+const number2 = Number(prompt('Введите второе число:'));
+
+const NOD = (num1, num2) => {
+  const min = num1 < num2 ? num1 : num2;
+  let res = 0;
+  for (let i = min; i >= 1; i--) {
+    if ((num1 % i === 0) && (num2 % i === 0)) {
+      return i;
+    }
+  }
+}
+
+console.log(NOD(number1, number2));
