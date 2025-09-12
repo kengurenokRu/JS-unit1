610e846afc03a2ef8a1d4118dcb3fd6e147dab9b
kengrenokRu <nastassiadunaeva@gmail.com>
2025-09-12 12:52:32 +0300

    add les4-2

--- les4/einstein.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const myWeight = 61;
-const speedLight = 3e8;
-
-console.log(`Энергия моего тела Е = ${speedLight * myWeight ** 2}`);
---
+++ les4/tasks.js
@@ -0,0 +1,14 @@
+const rain = Math.round(Math.random());
+
+if (rain) {
+  console.log('Пошёл дождь. Возьмите зонт!');
+}
+else {
+  console.log('Дождя нет!');
+}
+
+
+
+
+
+
