9d72ae1ec21d0bd5abbd32d82751ad72c54992e8
kengrenokRu <nastassiadunaeva@gmail.com>
2025-09-12 14:35:42 +0300

    add hard_task_02

---
+++ les5/hard_task_02.js
@@ -0,0 +1,6 @@
+const number1 = Number(prompt('Введите первое число:'));
+const number2 = Number(prompt('Введите второе число:'));
+
+const min = (num1, num2) => ((num1 + num2) - Math.abs(num1 - num2)) / 2;
+
+console.log(min (number1, number2));
