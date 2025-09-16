5642d8a7efcfabe71ed24bf047589538334d4724
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-09-16 16:19:19 +0300

    change variable names

--- les4/tasks.js
+++ les4/tasks.js
@@ -5,10 +5,10 @@ const rain = Math.round(Math.random());
 if (rain) { console.log('Пошёл дождь. Возьмите зонт!'); }
 else { console.log('Дождя нет!'); }
 
-const ex1 = Number(prompt('Введите кол-во баллов по математике:'));
-const ex2 = Number(prompt('Введите кол-во баллов по русскому языку:'));
-const ex3 = Number(prompt('Введите кол-во баллов по информатике:'));
-if (ex1 + ex2 + ex3 >= 265) { console.log('Поздравляю, вы поступили на бюджет!'); }
+const mathExam = Number(prompt('Введите кол-во баллов по математике:'));
+const rusExam = Number(prompt('Введите кол-во баллов по русскому языку:'));
+const informExam = Number(prompt('Введите кол-во баллов по информатике:'));
+if (mathExam + rusExam + informExam >= 265) { console.log('Поздравляю, вы поступили на бюджет!'); }
 else { console.log('Сожалеем, вы не поступили на бюджет!'); }
 
 const sum = Number(prompt('Введите сумму для снятия:'));
