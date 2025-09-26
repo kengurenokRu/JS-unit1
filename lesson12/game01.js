20e0c0ccb664f05058dd8427362b0b72f2378ae0
Asya Dunaeva <nastassiadunaeva@gmail.com>
2025-09-26 12:37:57 +0300

    fix: fix task game01

--- lesson12/game01.js
+++ lesson12/game01.js
@@ -2,21 +2,21 @@
 
 const checkResult = (number) => {
   const userInput = Number(prompt('Введите число от 1 до 100'));
-  if (userInput === 0) return;
+  if (userInput === 0) return 0;
   if (userInput === number) {
-    alert('Верно!'); return;
+    return 1;
   };
   if (userInput < number) {
-    alert('Больше!'); checkResult(number);
+    alert('Больше!'); return checkResult(number);
   } else {
-    alert('Меньше!'); checkResult(number);
+    alert('Меньше!'); return checkResult(number);
   }
 };
 
 const game = () => {
   const number = Math.floor(Math.random() * 100) + 1;
   console.log(number);
-  checkResult(number);
+  if (checkResult(number) === 1) alert('Верно!');
 };
 
 game();
