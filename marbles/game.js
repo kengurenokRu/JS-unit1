1a727bafc5a94cf79fb63876b74c0f314465f48c
kengrenokRu <nastassiadunaeva@gmail.com>
2025-10-01 23:22:13 +0300

    add structure of game.js

--- marbles/game.js
+++ marbles/game.js
@@ -1,1 +1,19 @@
+(() => {
 
+  function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+  };
+
+  const game = () => {
+
+
+    return function start() {
+
+      return start();
+    }
+  };
+
+  window.marbles = game;
+})();
